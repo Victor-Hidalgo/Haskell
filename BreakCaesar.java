f939865467f960e0bc9825cf963f23d83a253914
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-03-22 18:58:28 +0000

    Update BreakCaesar.java

--- BreakCaesar.java
+++ BreakCaesar.java
@@ -5,30 +5,91 @@ public class BreakCaesar{
 	public static void main (String args[]) {
 
 	System.out.println("Hola, soy bata");
+
+	String str = "La vieja";
 	
-	papirruqui(',', 3);
+	System.out.println("\n The length is: " + str.length());
+	System.out.println("hello");	
+	papirruqui("hello", -23);
+	System.out.println("aaba maleta");
+	count('t', "aaba maleta");
+
+	
 
 }
 
-public static void papirruqui (char ch, int shift){
+public static void papirruqui (String str, int shift){
 
-	char newChar = ' ';
-	int diff = ch - 'a';
-	int capdiff = ch - 'A';
+	while(shift < 0){
+		
+		shift = shift + 26;}	
 
-	if(diff >= 0 && diff < 26) {	
+	int i = 0;
+	char ch = ' ';
+	int diff = 0;
+	int capdiff = 0;
+	String encrypt = " ";
 
-		newChar = (char) ((ch - 'a' + shift) % 26 + 'a');
-	}
+	while(i<str.length()){
+
+		ch = str.charAt(i);
+		diff = ch - 'a';
+		capdiff = ch - 'A';	
+
+		if(diff >= 0 && diff < 26) {	
+
+			ch = (char) ((ch - 'a' + shift) % 26 + 'a');
+		}
  	
-	else if(capdiff >= 0 && diff < 26) {	
+		else if(capdiff >= 0 && capdiff < 26) {	
 
-		newChar = (char) ((ch - 'A' + shift) % 26 + 'A');
-	}
+			ch = (char) ((ch - 'A' + shift) % 26 + 'A');
+		}
 
-	else {newChar = ch;}
+		else {ch = ch;}
+		encrypt = encrypt + ch; i++;}
 
-	System.out.println(newChar);
+	System.out.println(encrypt);
 
 }
+
+public static void count (char bata, String str){
+
+	double appears = 0;
+	double letters = 0;
+	double freq = 0.0;
+	int j = 0;
+	int diff = 0;
+	int capdiff = 0;
+	char ch = ' ';	
+
+	while(j<str.length()){
+
+		ch = str.charAt(j);
+		diff = ch - 'a';
+		capdiff = ch - 'A';	
+
+		if(diff >= 0 && diff < 26) {	
+			
+			if(ch == bata){appears++;letters++;}
+
+			else{letters++;}
+		}
+ 	
+		else if(capdiff >= 0 && capdiff < 26) {	
+
+			if(ch == bata){appears++;letters++;}
+
+			else{letters++;}
+		}
+
+		else {ch = ch;}
+		j++;}
+
+	freq = appears/letters;
+
+	System.out.println("Appears " + appears + " times \n The frequency is " + freq); 
+	
+}
+
 }
