242d8d453aa89d3c2263b8c3379eb1c946b648ea
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-03-26 20:44:08 +0100

    Update BreakCaesar.java

--- BreakCaesar.java
+++ BreakCaesar.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.ArrayList;
 
 public class BreakCaesar{
 
@@ -18,9 +19,13 @@ public class BreakCaesar{
 	papu[0] = 3.4;
 	papu[1] = 8.12;
 
-	get_freqs("AdC");
+	double [] mom = {0.1, 0.9};
+	double [] dad = {0.8, 0.2};
+	chi_squared(mom, dad);
 
+	double[] knownFreq = {0.0855, 0.0160, 0.0316, 0.0387, 0.1210, 0.0218, 0.0209, 0.0496, 0.0733, 0.0022, 0.0081, 0.0421, 0.0253, 0.0717, 0.0747, 0.0207, 0.0010, 0.0633, 0.0673, 0.0894, 0.0268, 0.0106, 0.0183, 0.0019, 0.0172, 0.0011};
 
+	chi_squared(get_freqs("hello"), knownFreq);
 }
 
 public static void papirruqui (String str, int shift){
@@ -105,6 +110,7 @@ public static double frequency (char bata, String str){
 		capdiff = ch - 'A';
 
 		if((diff >= 0 && diff < 26) || (capdiff >= 0 && capdiff < 26)){letters++;}
+		else{letters = letters;}
 	j++;}
 
 	freq = count(bata, str) / letters;
@@ -112,7 +118,7 @@ public static double frequency (char bata, String str){
 	return freq;	
 }
 
-public static void get_freqs (String str){
+public static double[] get_freqs (String str){
 
 	int k = 0;
 	int i = 0;
@@ -125,9 +131,20 @@ public static void get_freqs (String str){
 	newChar = (char) (('a' - 'a' + i) % 26 + 'a');
 	freqs[k] = frequency(newChar, str);
 	
-	System.out.println(freqs[k]);
-	
 k++;i++;}
- 
-	}
+return freqs;}
+
+public static void chi_squared (double[] freq, double[] english){
+
+	int j = 0;
+	double chi = 0;
+
+	while (j < freq.length && j < english.length){
+	
+	double operation = (freq[j] - english[j]) * (freq[j] - english[j])/ english[j];
+	chi = chi + operation;
+	j++;	
+}
+	System.out.println(chi);
+}
 }
