e481487caf40c6740fda9d5ff1601b3580f6d0b5
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-04-18 23:15:46 +0100

    Add files via upload

---
+++ Cockroach.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public abstract class Cockroach{
+    
+    public abstract double takeStep();
+    
+    public abstract String name();
+    
+    public abstract double dice();
+}
