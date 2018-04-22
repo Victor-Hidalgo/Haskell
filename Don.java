c33da92d2e3b3020f767a0305657a713bd918539
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-04-22 14:28:43 +0100

    Update Don.java

--- Don.java
+++ Don.java
@@ -32,33 +32,92 @@ public class Don extends Cockroach{
     return walk;
     }
     
-    public double location(){
+    public double location(int times){
         
         double loc = 1;
-        double steps = takeStep();
-        double first = Math.random()*100;
-        double rounded = Math.round(first*10)/10.0;
+        int i = 0;
+        int j = 1;
+        int max = 0;
+        double[] numbers = new double[100];
+        double steps = 0;
+        double first = 0;
+        double rounded = 0;
         
-        if(rounded <= 0.1){
-            loc = 1;
-        }
-        
-        else{
+        while(i<times){
             
-            if(loc == 1 && steps<0){
-            loc = 1;
-            }
+            steps = takeStep();
+            first = Math.random()*100;
+            rounded = Math.round(first*10)/10.0;
         
-            else if(loc==102 && steps>0){
-                loc = 102;
+            if(rounded <= 0.1){
+                loc = 1;
             }
             
             else{
-                loc = loc + steps;
+                
+                if(loc == 1 && steps<0){
+                loc = 1;
+                }
+            
+                else if(loc==102 && steps>0){
+                    loc = 102;
+                }
+                
+                else{
+                    loc = loc + steps;
+                }
+                
             }
             
+            numbers[i] = loc;
+            
+            i++;
         }
         
-        return loc;
+        while(j<100){
+            
+            if(numbers[max]<numbers[j]){
+                max = j;
+            }
+            j++;
+        }
+        
+        return numbers[max];
     }
-} 
+    
+    public double topfloor(){
+        
+        double first = 0;
+        double rounded = 0;
+        double timestep = 0;
+        double loc = 1;
+        double steps = 0;
+        
+        while(loc<102){
+            
+            timestep = takeStep();
+            first = Math.random()*100;
+            rounded = Math.round(first*10)/10.0;
+        
+            if(rounded <= 0.1){
+                loc = 1;
+            }
+            
+            else{
+                
+                if(loc == 1 && timestep<0){
+                loc = 1;
+                }
+                
+                else{
+                    loc = loc + timestep;
+                }
+                
+            }
+            
+            steps++;
+        }
+        
+        return steps;
+    }
+}
