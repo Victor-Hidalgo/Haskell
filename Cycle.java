9956125cac157ccddde12ad9a54aaa38644132ff
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-04-23 21:48:32 +0100

    Add files via upload

---
+++ Cycle.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class Cycle extends Graph{
+    
+    public Cycle(){
+        super();
+    } 
+    
+    public int[] twonegatives(int size){
+        
+        int[] vertices = new int[size];
+        int i = 0;
+        
+        while(i<size){
+            
+            vertices[i] = 1;
+            i++;
+        }
+    
+        vertices[0] = -1;
+ 
+        vertices[size/2] = -1;
+        
+        return vertices;
+    }
+    
+    public int[] oneStep(int[] positions){
+        
+        Random choose = new Random();
+        int i = choose.nextInt(positions.length - 1);
+        int j = i + 1;
+        
+        if(positions[j] == null){
+            positions[j] = positions[0];
+            
+            if(positions[i] == 1 && positions[j] == 1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+            }
+            
+            else if(positions[i] == 1 && positions[j] == -1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+            }
+            
+            else if(positions[i] == -1 && positions[j] == -1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+            }
+            
+            else if(positions[i] == -1 && positions[j] == 1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+            }
+        }
+        
+        else if(positions[i] == 1 && positions[j] == 1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+        }
+        
+        else if(positions[i] == 1 && positions[j] == -1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+        }
+        
+        else if(positions[i] == -1 && positions[j] == -1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+        }
+        
+        else if(positions[i] == -1 && positions[j] == 1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+        }
+        
+        return positions;
+    }
+}
---
+++ Path.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Path extends Graph{
+    
+    public Path(){
+        super();
+    } 
+    
+    public int[] twonegatives(int size){
+        
+        int[] vertices = new int[size];
+        int i = 0;
+        
+        while(i<size){
+            
+            vertices[i] = 1;
+            i++;
+        }
+    
+        vertices[0] = -1;
+ 
+        vertices[size-1] = -1;
+        
+        return vertices;
+    }
+    
+    public int[] oneStep(int[] positions){
+        
+        Random choose = new Random();
+        int i = choose.nextInt(positions.length - 1);
+        int j = i + 1;
+        
+        if(positions[j] == null){
+            positions[i] = positions[i];
+        }
+        
+        else if(positions[i] == 1 && positions[j] == 1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+        }
+        
+        else if(positions[i] == 1 && positions[j] == -1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+        }
+        
+        else if(positions[i] == -1 && positions[j] == -1){
+            
+            positions[i] = 1;
+            positions[j] = 1;
+        }
+        
+        else if(positions[i] == -1 && positions[j] == 1){
+            
+            positions[i] = -1;
+            positions[j] = -1;
+        }
+        
+        return positions;
+    }
+}
