f43412d0c3d97f35712364dace7e7327842813f2
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-05-07 19:30:37 +0100

    Update Vending.java

--- Vending.java
+++ Vending.java
@@ -49,19 +49,31 @@ public class Vending extends JFrame{
             i++;
         }
         
-        items[0].setBounds(10, 20, 230, 25);
+        items[0].setBounds(10, 20, 220, 25);
         items[1].setBounds(380, 20, 210, 25);
-        items[2].setBounds(760, 20, 210, 25);
+        items[2].setBounds(740, 20, 210, 25);
         stock[0].setBounds(100, 50, 230, 25);
         stock[1].setBounds(475, 50, 230, 25);
         stock[2].setBounds(850, 50, 230, 25);
+        items[3].setBounds(10, 100, 220, 25);
+        items[4].setBounds(380, 100, 210, 25);
+        items[5].setBounds(740, 100, 220, 25);
+        stock[3].setBounds(100, 130, 230, 25);
+        stock[4].setBounds(475, 130, 230, 25);
+        stock[5].setBounds(850, 130, 230, 25);
+        items[6].setBounds(10, 180, 220, 25);
+        items[7].setBounds(380, 180, 210, 25);
+        items[8].setBounds(740, 180, 220, 25);
+        stock[6].setBounds(100, 210, 230, 25);
+        stock[7].setBounds(475, 210, 230, 25);
+        stock[8].setBounds(850, 210, 230, 25);
         
     }
     
     public static void main(String[] args){
         
         Vending frame = new Vending();
-        frame.setTitle("Downtown");
+        frame.setTitle("TwistyJigglyBomb surprises");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(200, 170, 1000 , 500);
         frame.setVisible(true);
