78b88982aeffe3f6c0add3d7ef52eeb63641ac0a
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-05-09 00:05:47 +0100

    Add files via upload

---
+++ Click.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Click implements ActionListener{
+    
+    JLabel[] will;
+    JButton[] stop;
+    int[] values;
+    
+    public Click(JButton[] items, JLabel[] stock, int[] amounts){
+        
+        will = stock;
+        stop = items;
+        values = amounts;
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        
+        int i = 0;
+        
+        while(i < stop.length){
+            
+            if (e.getSource() == stop[i]){
+                
+                values[i] = values[i] - 1;
+                
+                will[i].setText(values[i] + " left");
+            }
+            
+            i++;
+        }
+        
+        i = 0;
+    }
+}
