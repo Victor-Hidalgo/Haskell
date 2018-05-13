6fb402eb9f831a417af34b6e61e8eb5a6edbb112
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-05-13 18:29:36 +0100

    Update Doors.java

--- Doors.java
+++ Doors.java
@@ -5,5 +5,88 @@ public class Doors{
     
     public static void main (String args[]) {
         
+        int N = Integer.parseInt(args[0]);
+        int[] closed = new int[N+1];
+        int[] after;
+        int i = 0;
+        int open = 0;
+        
+        Ginny object1 = new Ginny();
+        Petra object2 = new Petra();
+        Sven object3 = new Sven();
+        
+        while(i<closed.length){
+            
+            closed[i] = 0;
+            i++;
+        }
+        
+        after = object1.toggling(closed, N);
+        i = 0;
+        
+        while(i<after.length){
+            
+            if(after[i] == 1){
+                
+                open++;
+            }
+            i++;
+        }
+        
+        i = 0;
+        
+        System.out.println(object1.name());
+        System.out.println(open + " doors open");
+        
+        while(i<closed.length){
+            
+            closed[i] = 0;
+            i++;
+        }
+        
+        after = object2.toggling(closed, N);
+        i = 0;
+        open = 0;
+        
+        while(i<after.length){
+            
+            if(after[i] == 1){
+                
+                open++;
+            }
+            i++;
+        }
+        
+        i = 0;
+        
+        System.out.println(object2.name());
+        System.out.println(open + " doors open");
+        
+        while(i<closed.length){
+            
+            closed[i] = 0;
+            i++;
+        }
+        
+        open = 0;
+        after = object3.toggling(closed, N);
+        i = 0;
+        
+        while(i<after.length){
+            
+            if(after[i] == 1){
+                
+                open++;
+            }
+            i++;
+        }
+        
+        i = 0;
+        
+        System.out.println(object3.name());
+        System.out.println(open + " doors open");
+        open = 0;
+        
     }
+    
 }
