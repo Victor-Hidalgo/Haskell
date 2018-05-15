ebd24fe3bbea6526c9a76a582574c643ebbdbccd
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-05-15 20:12:39 +0100

    Update Doors.java

--- Doors.java
+++ Doors.java
@@ -5,9 +5,9 @@ public class Doors{
     
     public static void main (String args[]) {
         
-        int N = Integer.parseInt(args[0]);
-        int[] closed = new int[N+1];
+        int N = 0;
         int[] after;
+        int[] closed = new int[1];
         int i = 0;
         int open = 0;
         
@@ -15,78 +15,200 @@ public class Doors{
         Petra object2 = new Petra();
         Sven object3 = new Sven();
         
-        while(i<closed.length){
+        if(args.length == 0){
             
-            closed[i] = 0;
-            i++;
+            System.out.println("Oops, not enough arguments");
         }
         
-        after = object1.toggling(closed, N);
-        i = 0;
-        
-        while(i<after.length){
+        else if(args.length == 1){
             
-            if(after[i] == 1){
+            try{
                 
-                open++;
+                N = Integer.parseInt(args[0]);
             }
-            i++;
-        }
-        
-        i = 0;
-        
-        System.out.println(object1.name());
-        System.out.println(open + " doors open");
-        
-        while(i<closed.length){
-            
-            closed[i] = 0;
-            i++;
-        }
-        
-        after = object2.toggling(closed, N);
-        i = 0;
-        open = 0;
-        
-        while(i<after.length){
-            
-            if(after[i] == 1){
                 
-                open++;
+            catch(NumberFormatException e){ //deals with doubles and any input which is not a number.
+                 
+                System.out.println("The argument must be a positive integer between 1 and 1000000");
             }
-            i++;
-        }
-        
-        i = 0;
-        
-        System.out.println(object2.name());
-        System.out.println(open + " doors open");
-        
-        while(i<closed.length){
             
-            closed[i] = 0;
-            i++;
-        }
-        
-        open = 0;
-        after = object3.toggling(closed, N);
-        i = 0;
-        
-        while(i<after.length){
-            
-            if(after[i] == 1){
+            if(N<1 || N>1000000){
                 
-                open++;
+                System.out.println("The argument must be a positive integer between 1 and 1000000");
             }
-            i++;
+            
+            else{
+                
+                try{
+                    closed = new int[N+1];
+                }
+                
+                catch(NegativeArraySizeException exception){ // deals with negative values.
+                
+                    System.out.println("The argument must be a positive integer between 1 and 1000000");
+                }
+            
+                while(i<closed.length){
+                    
+                    closed[i] = 0;
+                    i++;
+                }
+            
+                after = object1.toggling(closed, N);
+                i = 0;
+                open = 0;
+                
+                while(i<after.length){
+                    
+                    if(after[i] == 1){
+                        
+                        open++;
+                    }
+                    i++;
+                }
+            
+                i = 0;
+                
+                System.out.println(object1.name());
+                System.out.println(open + " doors open");
+                
+                while(i<closed.length){
+                    
+                    closed[i] = 0;
+                    i++;
+                }
+                
+                after = object2.toggling(closed, N);
+                i = 0;
+                open = 0;
+                
+                while(i<after.length){
+                    
+                    if(after[i] == 1){
+                        
+                        open++;
+                    }
+                    i++;
+                }
+                
+                i = 0;
+            
+                System.out.println(object2.name());
+                System.out.println(open + " doors open");
+                
+                while(i<closed.length){
+                    
+                    closed[i] = 0;
+                    i++;
+                }
+                
+                open = 0;
+                after = object3.toggling(closed, N);
+                i = 0;
+                
+                while(i<after.length){
+                    
+                    if(after[i] == 1){
+                        
+                        open++;
+                    }
+                    i++;
+                }
+                
+                i = 0;
+                
+                System.out.println(object3.name());
+                System.out.println(open + " doors open");
+                open = 0;
+            }
         }
         
-        i = 0;
-        
-        System.out.println(object3.name());
-        System.out.println(open + " doors open");
-        open = 0;
-        
+        else if(args.length == 2){
+            
+            try{
+                
+                N = Integer.parseInt(args[0]);
+            }
+                
+            catch(NumberFormatException e){ //deals with doubles and any input which is not a number.
+                 
+                System.out.println("The argument must be a positive integer between 1 and 1000000");
+            }
+            
+            if(N<1 || N>1000000){
+                
+                System.out.println("The argument must be a positive integer between 1 and 1000000");
+            }
+            
+            else{
+                
+                try{
+                    closed = new int[N+1];
+                }
+                
+                catch(NegativeArraySizeException exception){ // deals with negative values.
+                
+                    System.out.println("The argument must be a positive integer between 1 and 1000000");
+                }
+            
+                while(i<closed.length){
+                    
+                    closed[i] = 0;
+                    i++;
+                }
+                
+                open = 0;
+                i = 0;
+                String str = args[1];
+                char ch = ' ';
+                boolean wrong = false;
+                
+                while(i<str.length()){
+                    
+                    ch = str.charAt(i);
+                    
+                    if(ch == 'g' || ch == 'G'){
+                        
+                        closed = object1.toggling(closed, N);
+                    }
+                    
+                    else if(ch == 'p' || ch == 'P'){
+                        
+                        closed = object2.toggling(closed, N);
+                    }
+                    
+                    else if(ch == 's' || ch == 'S'){
+                        
+                        closed = object3.toggling(closed, N);
+                    }
+                    
+                    else{
+                        
+                        System.out.println("Invalid character");
+                        wrong = true;
+                        break;
+                    }
+                        
+                    i++;
+                }
+            
+                i = 0;
+                
+                while(i<closed.length){
+                    
+                    if(closed[i] == 1){
+                        
+                        open++;
+                    }
+                    i++;
+                }
+                
+                if(wrong == false){
+                
+                    System.out.println(open + " doors open");
+                }
+            }
+        }
     }
     
 }
